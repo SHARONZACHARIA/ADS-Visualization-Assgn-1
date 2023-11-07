9e15237eada67d8c4183e62aeb7327f93074fa25
Sharon Zacharia <zachariasharon@gmail.com>
2023-11-07 22:29:38 +0000

    added bar chart

--- nobel_prize_visualization.py
+++ nobel_prize_visualization.py
@@ -14,7 +14,8 @@ male_laureates_count = male_laureates.groupby("year").size()
 female_laureates = nobel_prize[nobel_prize["gender"] == "female"]
 female_laureates_count = female_laureates.groupby("year").size()
 
-"Filtering Top 5 countrues in which where laurates are born"
+
+"Filtering Top 5 countrues in which  laurates are born"
 lauretes_by_country = nobel_prize["born_country_code"].value_counts()
 top_5_countries = lauretes_by_country.head(5)
 
@@ -22,10 +23,20 @@ top_5_countries = lauretes_by_country.head(5)
 other_count = lauretes_by_country[5:].sum()
 top_5_countries['Other'] = other_count
 
+"Fetching the number of nobel prize  lauretes by age "
+age_counts = nobel_prize['age_get_prize'].value_counts().sort_index()
+
+def PlotBar(xyaxis):
+    xyaxis.plot(kind='bar')
+    plt.title("Number of Nobel Prize Laureates by Age")
+    plt.xlabel("Age")
+    plt.ylabel("Number of Laureates")
+    plt.show()
+
 
 
 def PlotLineGraph(male_count , female_count):
-    plt.plot(male_count,color='#5865f2')
+    plt.plot(male_count,color='#5865f2').groupby("year").size()
     plt.plot(female_count,color='#a60096')
     plt.xlabel('Year')
     plt.ylabel('Number of Laurets ')
@@ -37,5 +48,7 @@ def PlotPie(xyaxis):
     plt.pie(xyaxis, labels=xyaxis.index, autopct='%1.1f%%')
     plt.show()
 
+
 PlotPie(top_5_countries)
 PlotLineGraph(male_laureates_count,female_laureates_count)
+PlotBar(age_counts)
